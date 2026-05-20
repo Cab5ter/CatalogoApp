24d4ae96644165510b183172b53e3bff15f37357
cabster <leobalmes@hotmail.com>
2026-05-20 10:08:04 -0600

    CREATE: views - catalogo agregar, detalle y index

--- CatalogoApp.presentaion/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace CatalogoApp.presentaion.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
